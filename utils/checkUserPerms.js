4ea86f73cd3f6822832aa020794e3269a22d89ea
BluDood <bludood@bludood.com>
2022-11-02 12:38:52 +0100

    fix again oops

--- utils/checkUserPerms.js
+++ utils/checkUserPerms.js
@@ -2,6 +2,6 @@ const { PermissionFlagsBits } = require('discord.js')
 const { modRoles } = require('../config.json')
 module.exports = interaction => {
   const roles = interaction.member.roles.cache.map(r => r.id)
-  if (roles.some(id => modRoles.includes(parseInt(id))) || interaction.member.permissions.has(PermissionFlagsBits.Administrator)) return true
+  if (roles.some(id => modRoles.includes(id)) || interaction.member.permissions.has(PermissionFlagsBits.Administrator)) return true
   return false
 }
