bfe561e84d6175019d5b7e5d1715a140efe9d9b0
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2022-11-02 19:16:40 +0400

    Add tag command

---
+++ commands/tag.js
@@ -0,0 +1,82 @@
+// Make a tag command that has 4 subcommands: add, remove, list and get
+
+const { SlashCommandBuilder } = require( "discord.js" );
+const fs = require( "fs" );
+
+if ( !fs.existsSync( "./databases/tags.json" ) ) {
+    fs.writeFileSync( "./databases/tags.json", "{}" );
+}
+
+const tags = JSON.parse( fs.readFileSync( "./databases/tags.json" ) );
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName( "tag" )
+        .setDescription( "Manage tags" )
+        .addSubcommand( ( subcommand ) =>
+            subcommand
+                .setName( "add" )
+                .setDescription( "Add a tag" )
+                .addStringOption( ( option ) => option.setName( "name" ).setDescription( "The name of the tag" ).setRequired( true ) )
+                .addStringOption( ( option ) => option.setName( "content" ).setDescription( "The content of the tag" ).setRequired( true ) )
+        )
+        .addSubcommand( ( subcommand ) =>
+            subcommand
+                .setName( "remove" )
+                .setDescription( "Remove a tag" )
+                .addStringOption( ( option ) => option.setName( "name" ).setDescription( "The name of the tag" ).setRequired( true ) )
+        )
+        .addSubcommand( ( subcommand ) => subcommand.setName( "list" ).setDescription( "List all tags" ) )
+        .addSubcommand( ( subcommand ) =>
+            subcommand
+                .setName( "get" )
+                .setDescription( "Get a tag" )
+                .addStringOption( ( option ) => option.setName( "name" ).setDescription( "The name of the tag" ).setRequired( true ) )
+        ),
+
+    async execute ( interaction ) {
+        const subcommand = interaction.options.getSubcommand();
+        if ( subcommand === "add" ) {
+            if ( !checkUserPerms( interaction ) ) {
+                return interaction.reply( {
+                    content: "You do not have permission to do that!",
+                    ephemeral: true
+                } );
+            }
+            const name = interaction.options.getString( "name" );
+            const content = interaction.options.getString( "content" );
+            if ( tags[ name ] ) {
+                return interaction.reply( { content: `A tag with the name ${ name } already exists.`, ephemeral: true } );
+            }
+            tags[ name ] = content;
+            fs.writeFileSync( "./databases/tags.json", JSON.stringify( tags, null, 4 ) );
+            interaction.reply( { content: `Added tag ${ name }.`, ephemeral: true } );
+        } else if ( subcommand === "remove" ) {
+            if ( !checkUserPerms( interaction ) ) {
+                return interaction.reply( {
+                    content: "Just don't use that tag then ¯\\_(ツ)_/¯ (You don't have permission to do that.)",
+                    ephemeral: true
+                } );
+            }
+            const name = interaction.options.getString( "name" );
+            if ( !tags[ name ] ) {
+                return interaction.reply( { content: `A tag with the name ${ name } does not exist.`, ephemeral: true } );
+            }
+            delete tags[ name ];
+            fs.writeFileSync( "./databases/tags.json", JSON.stringify( tags, null, 4 ) );
+            interaction.reply( { content: `Removed tag ${ name }.`, ephemeral: true } );
+        } else if ( subcommand === "list" ) {
+            const tagList = Object.keys( tags ).join( ", " );
+            if ( tagList.length === 0 ) {
+                return interaction.reply( { content: "There are no tags.", ephemeral: true } );
+            }
+            interaction.reply( { content: tagList, ephemeral: true } );
+        } else if ( subcommand === "get" ) {
+            const name = interaction.options.getString( "name" );
+            if ( !tags[ name ] ) {
+                return interaction.reply( { content: `A tag with the name ${ name } does not exist.`, ephemeral: true } );
+            }
+            interaction.reply( { content: tags[ name ] } );
+        }
+    }
+};
