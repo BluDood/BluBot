2c798c69a5221aa4aba4a205c112a20257416950
BluDood <bludood@bludood.com>
2022-11-02 20:34:56 +0100

    Clean up listeners and add modal support

--- index.js
+++ index.js
@@ -16,10 +16,31 @@ const client = new Client({
 })
 
 client.commands = new Collection()
+client.modals = new Collection()
+client.autoComplete = new Collection()
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+const modalFiles = fs.readdirSync('./modals').filter(file => file.endsWith('.js'))
+const autoCompleteFiles = fs.readdirSync('./autocomplete').filter(file => file.endsWith('.js'))
+
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`)
   client.commands.set(command.data.name, command)
+}
+
+for (const file of modalFiles) {
+  const modal = require(`./modals/${file}`)
+  client.modals.set(modal.id, modal)
+}
+
+for (const file of autoCompleteFiles) {
+  const autoComplete = require(`./autocomplete/${file}`)
+  client.autoComplete.set(autoComplete.id, autoComplete)
+}
+
+for (const eventFile of fs.readdirSync('./events').filter(file => file.endsWith('.js'))) {
+  const event = require(`./events/${eventFile}`)
+  client.on(event.event, event.listener)
 }
 
 bconsole.init(process.argv[2])
@@ -29,34 +50,20 @@ client.once(Events.ClientReady, async c => {
   cacheAll(client)
 })
 
-for (const eventFile of fs.readdirSync('./events').filter(file => file.endsWith('.js'))) {
-  const event = require(`./events/${eventFile}`)
-  client.on(event.event, event.listener)
-}
-
-for (const autoCompleteFile of fs.readdirSync('./autocomplete').filter(file => file.endsWith('.js'))) {
-  const autoComplete = require(`./autocomplete/${autoCompleteFile}`)
-  client.on(Events.InteractionCreate, interaction => {
-    if (!interaction.isAutocomplete()) return;
-    if (interaction.commandName !== autoComplete.id) return;
-    autoComplete.execute(interaction)
-  })
-}
-
 client.on(Events.Error, error => {
   console.log(error)
 })
 
 client.on(Events.InteractionCreate, async interaction => {
-  if (interaction.isButton()) return;
-  if (interaction.isAutocomplete()) return;
-  const command = client.commands.get(interaction.commandName)
-  if (command) {
-    try {
-      await command.execute(interaction)
-    } catch (error) {
-      console.error(error)
-    }
+  if (interaction.isCommand()) {
+    const command = client.commands.get(interaction.commandName)
+    if (command) await command.execute(interaction).catch(console.error)
+  } else if (interaction.isModalSubmit()) {
+    const modal = client.modals.get(interaction.customId)
+    if (modal) await modal.execute(interaction).catch(console.error)
+  } else if (interaction.isAutocomplete()) {
+    const autoComplete = client.autoComplete.get(interaction.commandName)
+    if (autoComplete) await autoComplete.execute(interaction).catch(console.error)
   }
 })
 
